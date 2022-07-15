264da8fb6a0ca0cf2b6c983d08ee8789938dc1e1
CookieCrumbs19212 <developer19212@gmail.com>
2022-07-15 19:04:44 +0200

    Updated CNN architecture, removed maxpool layers.

--- architecture.py
+++ architecture.py
@@ -1,32 +1,49 @@
 import torch
 
 
-# noinspection SpellCheckingInspection
 class ImageInpaintingCNN(torch.nn.Module):
-    def __init__(self, n_in_channels: int = 1, n_hidden_layers: int = 3, n_kernels: int = 32, kernel_size: int = 7):
+    def __init__(self, num_input_channels: int = 4, num_hidden_layers: int = 5,
+                 num_kernels: int = 64, kernel_size: int = 3):
         super().__init__()
 
-        cnn = []
-        for i in range(n_hidden_layers):
-            cnn.append(torch.nn.Conv2d(
-                in_channels=n_in_channels,
-                out_channels=n_kernels,
-                kernel_size=kernel_size,
-                padding=int(kernel_size / 2)
-            ))
-            cnn.append(torch.nn.ReLU())
-            n_in_channels = n_kernels
-        self.hidden_layers = torch.nn.Sequential(*cnn)
+        # List to add NN layers.
+        layers = []
 
-        self.output_layer = torch.nn.Conv2d(
-            in_channels=n_in_channels,
-            out_channels=1,
-            kernel_size=kernel_size,
-            padding=int(kernel_size / 2)
-        )
+        """
+        One CNN block consists of 2 parts:
+        1. Convolution layer
+        2. Activation function (ReLU)
+        """
 
-    def forward(self, x):
-        """Apply CNN to input `x` of shape (N, n_channels, X, Y), where N=n_samples and X, Y are spatial dimensions"""
-        cnn_out = self.hidden_layers(x)  # apply hidden layers (N, n_in_channels, X, Y) -> (N, n_kernels, X, Y)
-        pred = self.output_layer(cnn_out)  # apply output layer (N, n_kernels, X, Y) -> (N, 1, X, Y)
-        return pred
+        # Loop to create N number of CNN blocks, where N = num_hidden_layers.
+        for i in range(num_hidden_layers):
+            # Add a 2D Convolutional layer.
+            layers.append(
+                torch.nn.Conv2d(
+                    in_channels=num_input_channels,
+                    out_channels=num_kernels,
+                    kernel_size=kernel_size,
+                    padding=int(kernel_size / 2)
+                )
+            )
+
+            # Add a ReLU activation function.
+            layers.append(
+                torch.nn.ReLU()
+            )
+
+            num_input_channels = num_kernels
+
+        # Compile the CNN blocks above into one sequential layer.
+        self.hidden_layers = torch.nn.Sequential(*layers)
+
+        # Creating output layer. Using Convolution layer.
+        self.output_layer = torch.nn.Conv2d(in_channels=num_input_channels,
+                                            out_channels=3,
+                                            kernel_size=kernel_size,
+                                            padding=int(kernel_size / 2))
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        cnn_out = self.hidden_layers(input_tensor)
+        predictions = self.output_layer(cnn_out)
+        return predictions
